7548051a2ae313967e605c369da61fb358cd1ccd
smhasnain678 <smhasnain43@gmail.com>
2026-09-07 20:19:41 +0500

    Basic steps

---
+++ log_analyzer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "==============================="
+echo " LINUX LOG ANALYZER AND SECURITY MONITOR"
+echo "==============================="
+
+echo "Hostname : $(hostname)"
+echo "Date     : $(date)"
+
+
+echo "==============================="
