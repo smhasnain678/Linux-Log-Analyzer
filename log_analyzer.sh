e550b5ebacd0173667c6d33966d7050a4b80fe35
smhasnain678 <smhasnain43@gmail.com>
2026-09-08 20:18:14 +0500

    Add log file validation and entry counting

--- log_analyzer.sh
+++ log_analyzer.sh
@@ -8,4 +8,27 @@ echo "Hostname : $(hostname)"
 echo "Date     : $(date)"
 
 
-echo "==============================================================="
+echo "======================================================"
+
+# Log Files
+#LOG_FILE="/var/log/auth.log"
+LOG_FILE="logs/sample_auth.log"
+
+echo "Analyzing Log Files: $LOG_FILE"
+
+# Check if log file exists
+if [ -f "$LOG_FILE" ]; then
+	echo "Log file found successfully."
+else
+	echo "ERROR: Log file not found!"
+	exit 1
+fi
+
+# Total Log Entries
+TOTAL_LOGS=$(wc -l < "$LOG_FILE")
+
+echo "Total Log Entries: $TOTAL_LOGS"
+
+echo "======================================================"
+
+
